0978e4ce1347675bf4eca046e8a215c6c6f94a3e
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2021-04-16 11:57:05 -0700

    Begin initializing relations TODO: 'Transactions' relations

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -43,9 +43,30 @@ db.sellers = require('./Seller.js')(sequelize, Sequelize)
 db.transactions = require('./Transaction.js')(sequelize, Sequelize)
 db.carts = require('./Cart.js')(sequelize, Sequelize)
 
-// Relations
+/*  Relations
+    https://sequelize.org/v3/docs/associations/
+*/
 
+/*
+    BelongsTo associations are associations where the foreign key for the one-to-one relation exists on the source model.
+*/
 
+/*
+    HasOne associations are associations where the foreign key for the one-to-one relation exists on the target model.
+*/
+db.customers.hasOne(db.carts, { foreignKey: 'customer_id' }) // cart is the target
+db.customers.hasOne(db.billing_info, { foreignKey: 'customer_id' })
+db.transactions.hasOne(db.customers, { foreignKey: 'customer_id' }) // a single customer will make a single transaction
+
+/*
+    One-To-Many associations are connecting one source with multiple targets. The targets however are again connected to exactly one specific source.
+*/
+db.sellers.hasMany(db.products, { foreignKey: 'seller_id' }) // a seller lists multiple products
+
+/*
+    Belongs-To-Many associations are used to connect sources with multiple targets. Furthermore the targets can also have connections to multiple sources.
+*/
+db.carts.belongsToMany(db.products, { foreignKey: 'product_id' }) // a product can be in multiple carts
 
 
 module.exports = db
