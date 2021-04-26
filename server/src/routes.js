53763ce7dd97c723ad0190a7b13a2d96a6d3f340
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2021-04-26 10:16:34 -0700

    Create AuthenticationControllerPolicy

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,7 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
     app.post('/api/register',
         AuthenticationControllerPolicy.register,
