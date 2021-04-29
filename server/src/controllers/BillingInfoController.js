5edbc45671f348a803d0698f9c0e6ae91766aec0
thomaspara <thomasjohnparashos@gmail.com>
2021-04-28 20:19:41 -0700

    adds api calls for more tables

---
+++ server/src/controllers/BillingInfoController.js
@@ -0,0 +1,42 @@
+const { BillingInfo } = require('../models')
+
+module.exports = {
+    async createBillingInfo (req, res) {
+        try {
+            console.log(req.body.billingInfo)
+            const billingInfo = await BillingInfo.create({
+                bank_num: req.body.billingInfo.bank_num,
+                customer_id: req.body.billingInfo.customer_id
+            })
+            res.status(200).send(billingInfo)
+        } catch (err) {
+            console.log(err)
+            res.status(400).send({
+                error: 'Error creating BillingInfo.'
+            })
+        }
+    },
+    async fetchBillingInfo (req, res) {
+        try {
+            const BILL_ID = req.body.bill_id
+            const BILLINGINFO = await BillingInfo.findOne({
+                where: {
+                    bill_id: BILL_ID
+                }
+            })
+            if (!BILLINGINFO) {
+                return res.status(404).send({
+                    error: 'Unable to fetch BillingInfo.'
+                })
+            } else {
+                res.send({
+                    billingInfo: BILLINGINFO
+                })
+            }
+        } catch (err) {
+            res.status(400).send({
+                error: 'Error trying to fetch BillingInfo.'
+            })
+        }
+    }
+}
