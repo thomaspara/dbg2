4f389a7a40266f9f5aacb15f53e64047d3c9255c
jazperez <jazmin.perez.634@my.csun.edu>
2021-05-03 18:17:08 -0700

    Add general css, build login and register page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,18 @@ export default {
 </script>
 
 <style>
+body, html {
+  padding: 2%;
+  margin: 0;
+  height: 100vh;
+}
+body {
+  background: linear-gradient(242.72deg, #AF38FB 30.06%, rgba(255, 255, 255, 0) 100%), #4583EE;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <h1 class="brand-name">Retail Store</h1>
+        <h2 class="page-name">Login</h2>
+        <form action="#">
+        <input class="form_input" type="text" name="email" placeholder="Email">
+        <input class="form_input" type="text" name="password" placeholder="Password">
+        </form>
+        <p class="login-link">Don't have an account? <a href="#">Register</a></p>
+        <div class="form_button">Login</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data () {
+        return {
+        msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
