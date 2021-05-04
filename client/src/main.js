533d10adee2ef9762b1f8b03aa14e7c0f395b0c6
jazperez <jazmin.perez.634@my.csun.edu>
2021-05-04 13:31:48 -0700

    Remove productcard component, fix home page and add products skeleton to home page

--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,11 @@ import App from './App'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './assets/styles/app.css'
 
-library.add(faShoppingCart)
+library.add(faShoppingCart, faPlusCircle)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
